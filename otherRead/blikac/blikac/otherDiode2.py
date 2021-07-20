88e053f751121ac3fe2fd84c9c22712095dcefb9
hrabeCesnek <sorinmarko@seznam.cz>
2021-07-20 23:59:23 +0100

    diodes

---
+++ otherRead/blikac/blikac/otherDiode2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 17:18:36 2019
+
+@author: Dan
+"""
+
+from zarizeni import pyf429
+from time import sleep
+
+led = pyf429(dev='/dev/ttyACM0')
+
+led.proud(5) # mA
+led.perioda(0.02) # ms
+led.sirkapulsu(10) # us
+led.LED3(0)
+led.LED2(1)
+led.LED1(0)
